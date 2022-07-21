d9f20d1125003ea205d392f458a947ea849786b7
Naveen-R-Mundaganur <105204623+Naveen-R-Mundaganur@users.noreply.github.com>
2022-07-21 16:27:21 +0530

    Update TypewiseAlert.cs

--- TypewiseAlert/TypewiseAlert.cs
+++ TypewiseAlert/TypewiseAlert.cs
@@ -4,30 +4,36 @@ namespace TypewiseAlert
 {
   public class TypewiseAlert
   {
-    public enum BreachType {
+    public enum BreachType 
+    {
       NORMAL,
       TOO_LOW,
       TOO_HIGH
     };
-    public static BreachType inferBreach(double value, double lowerLimit, double upperLimit) {
-      if(value < lowerLimit) {
+    public static BreachType inferBreach(double value, double lowerLimit, double upperLimit) 
+    {
+      if(value < lowerLimit) 
+      {
         return BreachType.TOO_LOW;
       }
-      if(value > upperLimit) {
+      if(value > upperLimit) 
+      {
         return BreachType.TOO_HIGH;
       }
       return BreachType.NORMAL;
     }
-    public enum CoolingType {
+    public enum CoolingType 
+    {
       PASSIVE_COOLING,
       HI_ACTIVE_COOLING,
       MED_ACTIVE_COOLING
     };
-    public static BreachType classifyTemperatureBreach(
-        CoolingType coolingType, double temperatureInC) {
+    public static BreachType classifyTemperatureBreach(CoolingType coolingType, double temperatureInC) 
+    {
       int lowerLimit = 0;
       int upperLimit = 35;
-      switch(coolingType) {
+      switch(coolingType) 
+      {
         //case CoolingType.PASSIVE_COOLING:
         //  lowerLimit = 0;
         //  upperLimit = 35;
@@ -43,22 +49,23 @@ namespace TypewiseAlert
       }
       return inferBreach(temperatureInC, lowerLimit, upperLimit);
     }
-    public enum AlertTarget{
+    public enum AlertTarget
+    {
       TO_CONTROLLER,
       TO_EMAIL
     };
-    public struct BatteryCharacter {
+    public struct BatteryCharacter 
+    {
       public CoolingType coolingType;
       public string brand;
     }
-    public static void checkAndAlert(
-        AlertTarget alertTarget, BatteryCharacter batteryChar, double temperatureInC) {
+    public static void checkAndAlert(AlertTarget alertTarget, BatteryCharacter batteryChar, double temperatureInC) 
+    {
 
-      BreachType breachType = classifyTemperatureBreach(
-        batteryChar.coolingType, temperatureInC
-      );
+      BreachType breachType = classifyTemperatureBreach(batteryChar.coolingType, temperatureInC);
 
-      switch(alertTarget) {
+      switch(alertTarget) 
+      {
         case AlertTarget.TO_CONTROLLER:
           sendToController(breachType);
           break;
@@ -67,25 +74,40 @@ namespace TypewiseAlert
           break;
       }
     }
-    public static void sendToController(BreachType breachType) {
+    //Send Message to Controller
+    public static void sendToController(BreachType breachType) 
+    {
       const ushort header = 0xfeed;
       Console.WriteLine("{} : {}\n", header, breachType);
     }
-    public static void sendToEmail(BreachType breachType) {
+    
+    //Send Email to Client
+    public static string sendToEmail(BreachType breachType) 
+    {
       string recepient = "a.b@c.com";
-      switch(breachType) {
-        case BreachType.TOO_LOW:
-          Console.WriteLine("To: {}\n", recepient);
-          Console.WriteLine("Hi, the temperature is too low\n");
-          break;
-        case BreachType.TOO_HIGH:
-          Console.WriteLine("To: {}\n", recepient);
-          Console.WriteLine("Hi, the temperature is too high\n");
-          break;
-        default: 
-          //BreachType.NORMAL Condition;
-          break;
+      string message = $"Hi, the temperature is ";
+      string alerttype = "normal";
+
+      if(breachType==BreachType.TOO_LOW)
+      {
+          alerttype = "too low";          
       }
+      else if(breachType==BreachType.TOO_HIGH)
+      {
+          alerttype = "too high";
+      }
+      else 
+      {
+          alerttype = "normal";
+      }
+      SendMessage(recepient, message, alerttype));
+      return alerttype;
     }
+    
+    //Send the Message in a Formated text
+     public static void SendMessage(string sender, string message_info, string alert_info)
+     {
+         Console.WriteLine($"To: {sender}\n {message_info}{alert_info}");            
+     } 
  }
 }
