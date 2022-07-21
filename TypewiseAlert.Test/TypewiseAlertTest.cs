33c18492cf44b120ca9e3c92c7df4572d0926153
Naveen-R-Mundaganur <105204623+Naveen-R-Mundaganur@users.noreply.github.com>
2022-07-21 15:49:41 +0530

    Update TypewiseAlertTest.cs

--- TypewiseAlert.Test/TypewiseAlertTest.cs
+++ TypewiseAlert.Test/TypewiseAlertTest.cs
@@ -10,14 +10,18 @@ namespace TypewiseAlert.Test
     {
       Assert.True(TypewiseAlert.inferBreach(12, 20, 30) ==
         TypewiseAlert.BreachType.TOO_LOW);
+      Assert.True(TypewiseAlert.inferBreach(-12, 20, 30) ==
+        TypewiseAlert.BreachType.TOO_LOW);
       Assert.True(TypewiseAlert.inferBreach(20, 20, 30) ==
-        TypewiseAlert.BreachType.TOO_LOW);
+        TypewiseAlert.BreachType.NORMAL);
       Assert.True(TypewiseAlert.inferBreach(50, 20, 30) ==
-        TypewiseAlert.BreachType.TOO_LOW);
+        TypewiseAlert.BreachType.TOO_HIGH);
       Assert.True(TypewiseAlert.inferBreach(30, 20, 30) ==
-        TypewiseAlert.BreachType.TOO_HIGH);
-      Assert.True(TypewiseAlert.inferBreach(50, 20, 30) ==
-        TypewiseAlert.BreachType.TOO_HIGH);
+        TypewiseAlert.BreachType.NORMAL);
+      Assert.True(TypewiseAlert.inferBreach(50, 50, 50) ==
+        TypewiseAlert.BreachType.NORMAL);
+      Assert.True(TypewiseAlert.inferBreach(25, 20, 40) ==
+        TypewiseAlert.BreachType.NORMAL);
     }
   }
 }
