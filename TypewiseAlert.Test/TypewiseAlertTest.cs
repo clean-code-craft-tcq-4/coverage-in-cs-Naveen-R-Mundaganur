111bc4ec313a46401d7b9b387a4244a4a554628a
Naveen-R-Mundaganur <105204623+Naveen-R-Mundaganur@users.noreply.github.com>
2022-07-21 22:20:18 +0530

    Update TypewiseAlertTest.cs

--- TypewiseAlert.Test/TypewiseAlertTest.cs
+++ TypewiseAlert.Test/TypewiseAlertTest.cs
@@ -8,67 +8,47 @@ namespace TypewiseAlert.Test
     [Fact]
     public void InfersBreachAsPerLimits()
     {
-      Assert.True(TypewiseAlert.inferBreach(12, 20, 30) ==
-        TypewiseAlert.BreachType.TOO_LOW);
-      Assert.True(TypewiseAlert.inferBreach(-12, 20, 30) ==
-        TypewiseAlert.BreachType.TOO_LOW);
-      Assert.True(TypewiseAlert.inferBreach(20, 20, 30) ==
-        TypewiseAlert.BreachType.NORMAL);
-      Assert.True(TypewiseAlert.inferBreach(50, 20, 30) ==
-        TypewiseAlert.BreachType.TOO_HIGH);
-      Assert.True(TypewiseAlert.inferBreach(30, 20, 30) ==
-        TypewiseAlert.BreachType.NORMAL);
-      Assert.True(TypewiseAlert.inferBreach(50, 50, 50) ==
-        TypewiseAlert.BreachType.NORMAL);
-      Assert.True(TypewiseAlert.inferBreach(25, 20, 40) ==
-        TypewiseAlert.BreachType.NORMAL);      
+      Assert.True(BatteryTemperatureAlert.inferBreach(12, 20, 30) ==
+        BatteryTemperatureAlert.BreachType.TOO_LOW);
+      Assert.True(BatteryTemperatureAlert.inferBreach(-12, 20, 30) ==
+        BatteryTemperatureAlert.BreachType.TOO_LOW);
+      Assert.True(BatteryTemperatureAlert.inferBreach(20, 20, 30) ==
+        BatteryTemperatureAlert.BreachType.NORMAL);
+      Assert.True(BatteryTemperatureAlert.inferBreach(50, 20, 30) ==
+        BatteryTemperatureAlert.BreachType.TOO_HIGH);
+      Assert.True(BatteryTemperatureAlert.inferBreach(30, 20, 30) ==
+        BatteryTemperatureAlert.BreachType.NORMAL);
+      Assert.True(BatteryTemperatureAlert.inferBreach(50, 50, 50) ==
+        BatteryTemperatureAlert.BreachType.NORMAL);
+      Assert.True(BatteryTemperatureAlert.inferBreach(25, 20, 40) ==
+        BatteryTemperatureAlert.BreachType.NORMAL);      
     }
      //Email Validation
     [Fact]
     public void SendsEmailBasedonBreach()
     {
      
-      Assert.True(TypewiseAlert.sendToEmail(TypewiseAlert.BreachType.TOO_LOW)=="too low");
-      Assert.True(TypewiseAlert.sendToEmail(TypewiseAlert.BreachType.TOO_HIGH)=="too high");
-      Assert.True(TypewiseAlert.sendToEmail(TypewiseAlert.BreachType.NORMAL)=="normal");  
+      Assert.True(BatteryTemperatureAlert.sendToEmail(BatteryTemperatureAlert.BreachType.TOO_LOW)=="too low");
+      Assert.True(BatteryTemperatureAlert.sendToEmail(BatteryTemperatureAlert.BreachType.TOO_HIGH)=="too high");
+      Assert.True(BatteryTemperatureAlert.sendToEmail(BatteryTemperatureAlert.BreachType.NORMAL)=="normal");  
     }
     
     //Classify the Breach Type Verification
      [Fact]
     public void ValidateBreachType()
     {
-      Assert.True(TypewiseAlert.classifyTemperatureBreach(TypewiseAlert.CoolingType.PASSIVE_COOLING, 22)==TypewiseAlert.BreachType.NORMAL);
-      Assert.True(TypewiseAlert.classifyTemperatureBreach(TypewiseAlert.CoolingType.HI_ACTIVE_COOLING, 48)==TypewiseAlert.BreachType.TOO_HIGH);
-      Assert.True(TypewiseAlert.classifyTemperatureBreach(TypewiseAlert.CoolingType.MED_ACTIVE_COOLING, -10)==TypewiseAlert.BreachType.TOO_LOW);                  
+      Assert.True(BatteryTemperatureAlert.classifyTemperatureBreach(BatteryTemperatureAlert.CoolingType.PASSIVE_COOLING, 22)==BatteryTemperatureAlert.BreachType.NORMAL);
+      Assert.True(BatteryTemperatureAlert.classifyTemperatureBreach(BatteryTemperatureAlert.CoolingType.HI_ACTIVE_COOLING, 48)==BatteryTemperatureAlert.BreachType.TOO_HIGH);
+      Assert.True(BatteryTemperatureAlert.classifyTemperatureBreach(BatteryTemperatureAlert.CoolingType.MED_ACTIVE_COOLING, -10)==BatteryTemperatureAlert.BreachType.TOO_LOW);                  
     }
     
     //Alert Check
     [Fact]
     public void CheckAndAlertTheBreach()
     {     
-        TypewiseAlert.checkAndAlert(TypewiseAlert.AlertTarget.TO_CONTROLLER, new TypewiseAlert.BatteryCharacter(TypewiseAlert.CoolingType.PASSIVE_COOLING),58.5d);
-        TypewiseAlert.checkAndAlert(TypewiseAlert.AlertTarget.TO_EMAIL, new TypewiseAlert.BatteryCharacter(TypewiseAlert.CoolingType.HI_ACTIVE_COOLING),42);
-        TypewiseAlert.checkAndAlert(TypewiseAlert.AlertTarget.TO_EMAIL, new TypewiseAlert.BatteryCharacter(TypewiseAlert.CoolingType.MED_ACTIVE_COOLING),-500);
-      /* try
-      {
-          TypewiseAlert.checkAndAlert(TypewiseAlert.AlertTarget.TO_CONTROLLER, new TypewiseAlert.BatteryCharacter(TypewiseAlert.CoolingType.PASSIVE_COOLING),58.5d);
-          Assert.True(true);
-      }
-      catch 
-      {
-          Assert.True(false);
-      }
-
-      try
-      {
-          TypewiseAlert.checkAndAlert(TypewiseAlert.AlertTarget.TO_EMAIL, new TypewiseAlert.BatteryCharacter(TypewiseAlert.CoolingType.HI_ACTIVE_COOLING),42);
-          Assert.True(true);
-      }
-      catch 
-      {
-          Assert.True(false);
-      }
-      */
+        BatteryTemperatureAlert.checkAndAlert(BatteryTemperatureAlert.AlertTarget.TO_CONTROLLER, new BatteryTemperatureAlert.BatteryCharacter(BatteryTemperatureAlert.CoolingType.PASSIVE_COOLING),58.5d);
+        BatteryTemperatureAlert.checkAndAlert(BatteryTemperatureAlert.AlertTarget.TO_EMAIL, new BatteryTemperatureAlert.BatteryCharacter(BatteryTemperatureAlert.CoolingType.HI_ACTIVE_COOLING),42);
+        BatteryTemperatureAlert.checkAndAlert(BatteryTemperatureAlert.AlertTarget.TO_EMAIL, new BatteryTemperatureAlert.BatteryCharacter(BatteryTemperatureAlert.CoolingType.MED_ACTIVE_COOLING),-500);      
     }
   }
 }
